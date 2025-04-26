eda802c1cbfa5ab682447fa98986ab707c8fa0fa
fdoliv <flpdias14@gmail.com>
2025-04-26 17:46:56 -0300

    test: add input validation test files

---
+++ src/test/java/com/axreng/backend/validators/KeywordValidatorTest.java
@@ -0,0 +1,37 @@
+package com.axreng.backend.validators;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+/**
+ * Test class for validating the functionality of the KeywordValidator.
+ * 
+ * This class uses parameterized tests with data provided from a CSV file
+ * to ensure that the KeywordValidator correctly validates search keywords.
+ * 
+ * The CSV file should contain test cases with two columns:
+ * - The first column represents the search keyword to be validated.
+ * - The second column represents the expected validation result (true for valid, false for invalid).
+ * 
+ * A search term is considered valid if it has a minimum of 4 characters and a maximum of 32 characters.
+ * Otherwise, it is considered invalid.
+ * 
+ * Each test case is executed to verify that the validation logic behaves as expected.
+ */
+public class KeywordValidatorTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/keyword_test_cases.csv", numLinesToSkip = 1)
+    @DisplayName("Should validate keywords correctly from CSV file")
+    void shouldValidateKeywordsFromCsv(String keyword, boolean expected) {
+        // When
+        boolean isValid = KeywordValidator.validate(keyword);
+
+        // Then
+        assertThat("Validation failed for keyword: " + keyword, isValid, is(expected));
+    }
+}
---
+++ src/test/java/com/axreng/backend/validators/SearchIDValidatorTest.java
@@ -0,0 +1,38 @@
+package com.axreng.backend.validators;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+/**
+ * Test class for validating the functionality of the SearchIDValidator.
+ * 
+ * This class uses parameterized tests with data provided from a CSV file
+ * to ensure that the SearchIDValidator correctly validates IDs.
+ * 
+ * The CSV file should contain test cases with two columns:
+ * - The first column represents the ID to be validated.
+ * - The second column represents the expected validation result (true for valid, false for invalid).
+ * 
+ * An ID is considered valid if it consists of exactly 8 alphanumeric characters.
+ * Otherwise, it is considered invalid.
+ * 
+ * Each test case is executed to verify that the validation logic behaves as expected.
+ */
+public class SearchIDValidatorTest {
+
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/id_test_cases.csv", numLinesToSkip = 1)
+    @DisplayName("Should validate ids correctly from CSV file")
+    void shouldValidateKeywordsFromCsv(String id, boolean expected) {
+        // When
+        boolean isValid = SearchIDValidator.validate(id);
+
+        // Then
+        assertThat("Validation failed for id: " + id, isValid, is(expected));
+    }
+}
