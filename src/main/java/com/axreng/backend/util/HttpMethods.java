c6f4625255f58bc3222ded819ec4177d55541127
fdoliv <flpdias14@gmail.com>
2025-04-27 20:09:32 -0300

    feat(HttpMethods): add utility class for HTTP method constants

---
+++ src/main/java/com/axreng/backend/util/HttpMethods.java
@@ -0,0 +1,8 @@
+package com.axreng.backend.util;
+
+public class HttpMethods {
+    public static final String POST = "POST";
+    public static final String GET = "GET";
+    public static final String PUT = "PUT";
+    public static final String DELETE = "DELETE";
+}
