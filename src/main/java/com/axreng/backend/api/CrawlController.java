ab7f0434de22e88ddad030d3e02066518282a855
fdoliv <flpdias14@gmail.com>
2025-04-27 19:27:49 -0300

    feat(CrawlController): implement GET endpoint for crawl status by ID with error handling feat(HttpResponseCode): add HTTP response codes as constants for better readability test(CrawlControllerTest): add tests for GET endpoint including various response scenarios

--- src/main/java/com/axreng/backend/api/CrawlController.java
+++ src/main/java/com/axreng/backend/api/CrawlController.java
@@ -2,12 +2,15 @@ package com.axreng.backend.api;
 
 import static spark.Spark.*;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import com.axreng.backend.exception.KeywordValidatorException;
+import com.axreng.backend.exception.SearchIDValidatorException;
 import com.axreng.backend.model.Search;
 import com.axreng.backend.service.SearchService;
 import com.axreng.backend.util.KeywordValidator;
+import com.axreng.backend.util.SearchIDValidator;
 import com.google.gson.Gson;
 
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CrawlController {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(CrawlController.class);
 
     private static final Gson GSON = new Gson();
@@ -36,6 +40,56 @@ public class CrawlController {
     
     public void initializeRoutes() {
 
+
+        get("/crawl/:id", (req, res) -> {
+            res.type("application/json");
+
+            try {
+                String id = req.params("id");
+                SearchIDValidator.validate(id);
+                Optional<Search> optionalSearch = searchService.findSearchById(id);
+                if (optionalSearch.isPresent()) {
+
+                    CrawlStatusResponse crawlStatusResponse = new CrawlStatusResponse();
+                    crawlStatusResponse.setId(optionalSearch.get().getId());
+                    crawlStatusResponse.setStatus(optionalSearch.get().getStatus().getValue());
+                    crawlStatusResponse.setUrls(new ArrayList<>(optionalSearch.get().getUrls()));    
+                    res.status(HttpServletResponse.SC_OK);
+                    return GSON.toJson(crawlStatusResponse, CrawlStatusResponse.class);
+                }
+                res.status(HttpServletResponse.SC_NOT_FOUND);
+                return GSON.toJson(new CrawlErrorResponse(
+                        HttpServletResponse.SC_NOT_FOUND, 
+                        "Not Found", 
+                        "Search with ID " + id + " not found", 
+                        req.pathInfo()
+                ));
+                
+                
+            } catch (SearchIDValidatorException sive){
+                res.status(HttpServletResponse.SC_BAD_REQUEST);
+
+                return GSON.toJson(new CrawlErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST, 
+                    "Bad Request", 
+                    sive.getMessage(), 
+                    req.pathInfo()
+                ));
+            }
+            
+            catch (Exception e) {
+                LOGGER.error("An unexpected error occurred", e);
+                res.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+
+                return GSON.toJson(new CrawlErrorResponse(
+                    HttpServletResponse.SC_INTERNAL_SERVER_ERROR, 
+                    "Internal Server Error", 
+                    "", 
+                    req.pathInfo()
+                ));
+            }
+        });
+  
         post("/crawl", (req, res) ->{
 
             res.type("application/json");
@@ -83,6 +137,8 @@ public class CrawlController {
                 ));
             }
         });
+
+        
     }
 
 }
--- src/test/java/com/axreng/backend/api/CrawlControllerTest.java
+++ src/test/java/com/axreng/backend/api/CrawlControllerTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.axreng.backend.helper.HttpClientHelper;
+import com.axreng.backend.helper.JsonHelper;
+import com.axreng.backend.util.HttpResponseCode;
 
 import java.net.HttpURLConnection;
 
@@ -18,14 +20,13 @@ class CrawlControllerTest {
 
     @BeforeAll
     static void setUp() {
-        
         CrawlController.getInstance().initializeRoutes();
-        spark.Spark.awaitInitialization(); 
+        spark.Spark.awaitInitialization();
     }
 
     @AfterAll
     static void tearDown() {
-        spark.Spark.stop(); 
+        spark.Spark.stop();
     }
 
     @Test
@@ -64,7 +65,7 @@ class CrawlControllerTest {
     @DisplayName("Should return 400 Bad Request for missing keyword")
     void shouldReturn400ForMissingKeyword() throws Exception {
         // Given
-        String requestBody = "{ }"; 
+        String requestBody = "{ }";
         HttpURLConnection connection = helper.createConnection("http://localhost:4567/crawl", "POST", requestBody);
 
         // When
@@ -74,5 +75,102 @@ class CrawlControllerTest {
         // Then
         assertThat(responseCode, is(400));
         assertThat(responseBody, containsString("Invalid keyword"));
+    }
+
+    @Test
+    @DisplayName("Should return Content-Type as application/json")
+    void shouldReturnContentTypeAsApplicationJson() throws Exception {
+        // Given
+        String requestBody = "{ \"keyword\": \"validKeyword\" }";
+        HttpURLConnection connection = helper.createConnection("http://localhost:4567/crawl", "POST", requestBody);
+
+        // When
+        connection.getResponseCode();
+        String contentType = connection.getHeaderField("Content-Type");
+
+        // Then
+        assertThat("Content-type shoud be application/json", contentType, is("application/json"));
+    }
+
+    @Test
+    @DisplayName("Should return 200 OK for valid ID")
+    void shouldReturn200ForValidId() throws Exception {
+        // Given
+        String requestBody = "{ \"keyword\": \"validKeyword\" }";
+        HttpURLConnection postConnection = helper.createConnection("http://localhost:4567/crawl", "POST", requestBody);
+        postConnection.getResponseCode();
+        String postResponseBody = helper.readResponse(postConnection);
+        String id = JsonHelper.extractFieldFromJson(postResponseBody, "id");
+
+        // When
+        HttpURLConnection getConnection = helper.createConnection("http://localhost:4567/crawl/" + id, "GET", null);
+        int responseCode = getConnection.getResponseCode();
+        String contentType = getConnection.getHeaderField("Content-Type");
+
+
+        // Then
+        assertThat("Response code should indicate OK code", responseCode, is(HttpResponseCode.OK));
+        assertThat("Content-type shoud be application/json", contentType, is("application/json"));
+    }
+
+    @Test
+    @DisplayName("Should return 400 Bad Request for short ID")
+    void shouldReturn400ForShortId() throws Exception {
+        // When
+        HttpURLConnection connection = helper.createConnection("http://localhost:4567/crawl/1342", "GET", null);
+        int responseCode = connection.getResponseCode();
+        String responseBody = helper.readResponse(connection);
+        String contentType = connection.getHeaderField("Content-Type");
+
+        // Then
+        assertThat("Response code should indicate BAD_REQUEST code", responseCode, is(HttpResponseCode.BAD_REQUEST));
+        assertThat("Response message should indicate that id is too short", responseBody, containsString("too short"));
+        assertThat("Content-type shoud be application/json", contentType, is("application/json"));
+    }
+
+    @Test
+    @DisplayName("Should return 400 Bad Request for long ID")
+    void shouldReturn400ForLongId() throws Exception {
+        // When
+        HttpURLConnection connection = helper.createConnection("http://localhost:4567/crawl/longidsended", "GET", null);
+        int responseCode = connection.getResponseCode();
+        String responseBody = helper.readResponse(connection);
+        String contentType = connection.getHeaderField("Content-Type");
+
+        // Then
+        assertThat("Response code should indicate BAD_REQUEST code", responseCode, is(HttpResponseCode.BAD_REQUEST));
+        assertThat("Response message should indicate that id is too long", responseBody, containsString("too long"));
+        assertThat("Content-type shoud be application/json", contentType, is("application/json"));
+    }
+
+    @Test
+    @DisplayName("Should return 404 Not Found for non-existent ID")
+    void shouldReturn404ForNonExistentId() throws Exception {
+        
+        // When
+        HttpURLConnection connection = helper.createConnection("http://localhost:4567/crawl/1234abcd", "GET", null);
+        int responseCode = connection.getResponseCode();
+        String responseBody = helper.readResponse(connection);
+        String contentType = connection.getHeaderField("Content-Type");
+
+        // Then
+        assertThat("Response code should indicate NOT_FOUND code", responseCode, is(HttpResponseCode.NOT_FOUND));
+        assertThat("Response message should indicate that id is not found", responseBody, containsString("not found"));
+        assertThat("Content-type shoud be application/json", contentType, is("application/json"));
+    }
+
+    @Test
+    @DisplayName("Should return 400 Bad Request for ID with non-alphanumeric characters")
+    void shouldReturn400ForNonAlphanumericId() throws Exception {
+        // When
+        HttpURLConnection connection = helper.createConnection("http://localhost:4567/crawl/123$abcd", "GET", null);
+        int responseCode = connection.getResponseCode();
+        String responseBody = helper.readResponse(connection);
+        String contentType = connection.getHeaderField("Content-Type");
+
+        // Then
+        assertThat("Response code should indicate BAD_REQUEST", responseCode, is(400));
+        assertThat("Response message should indicate that id must be alphanumeric",responseBody, containsString("must be alphanumeric"));
+        assertThat("Content-type shoud be application/json", contentType, is("application/json"));
     }
 }
