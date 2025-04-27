bf7aae46eb08794f0dd282d7fbd322b36876f30b
fdoliv <flpdias14@gmail.com>
2025-04-27 17:01:24 -0300

    feat(logging): add logging to Main class for application startup

--- src/main/java/com/axreng/backend/Main.java
+++ src/main/java/com/axreng/backend/Main.java
@@ -1,10 +1,15 @@
 package com.axreng.backend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.axreng.backend.api.CrawlController;
 
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    
     public static void main(String[] args) {
-
+        LOGGER.info("Starting the application...");
         CrawlController.getInstance().initializeRoutes();
 
     }
