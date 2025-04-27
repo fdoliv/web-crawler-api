b6559dfa793807dc9a0a9d088b9238845bc102ee
fdoliv <flpdias14@gmail.com>
2025-04-27 14:16:44 -0300

    feat(repository): implement SearchRepository with CRUD operations and add SearchRepositoryTest for validation

--- src/main/java/com/axreng/backend/model/Search.java
+++ src/main/java/com/axreng/backend/model/Search.java
@@ -1,5 +1,6 @@
 package com.axreng.backend.model;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class Search {
@@ -9,11 +10,43 @@ public class Search {
     Status status;     
     private Set<String> urls;
 
+    public Search(String id, String keyword, Status status) {
+        this.id = id;
+        this.keyword = keyword;
+        this.status = status;
+    }
+
+    public Search(String id) {
+        this.id = id;
+    }
+
     public Set<String> getUrls() {
         return urls;
     }
 
     public void setUrls(Set<String> urls) {
         this.urls = urls;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Search search = (Search) o;
+        return Objects.equals(id, search.id) &&
+               Objects.equals(keyword, search.keyword) &&
+               Objects.equals(status, search.status);
     }
 }
