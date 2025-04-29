2267ab159fc658b184f2772fad91deeecaa57b5c
fdoliv <flpdias14@gmail.com>
2025-04-29 01:55:48 -0300

    feat(Search): add setter for status field

--- src/main/java/com/axreng/backend/model/Search.java
+++ src/main/java/com/axreng/backend/model/Search.java
@@ -45,6 +45,10 @@ public class Search {
         return status;
     }
 
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
      @Override
     public boolean equals(Object o) {
         if (this == o) return true;
